089265b81a6a9a4da321bb593d8caebd581c4561
Aleksey Uymanov <s9gf4ult@gmail.com>
2020-07-02 00:17:35 +0500

    interesting

--- Projects.v
+++ Projects.v
@@ -5,10 +5,6 @@ Inductive Project : Type :=
 | Prod : Project -> Project -> Project
 .
 
-Notation "a '+' b" := (Sum a b).
-
-Notation "a '*' b" := (Prod a b).
-
 Inductive Exec : Project -> Prop :=
 | ETask : forall n, Exec (Task n)
 | ESumLeft : forall a b, Exec a -> Exec (Sum a b)
@@ -16,22 +12,30 @@ Inductive Exec : Project -> Prop :=
 | EProd : forall a b, Exec a -> Exec b -> Exec (Prod a b)
 .
 
-Fixpoint execLeft (p : Project) : Exec p :=
-  match p with
-  | Task n => ETask n
-  | Sum a b => ESumLeft a b (execLeft a)
-  | Prod a b => EProd a b (execLeft a) (execLeft b)
-  end
-  .
+(* Fixpoint execLeft (p : Project) : Exec p := *)
+(*   match p with *)
+(*   | Task n => ETask n *)
+(*   | Sum a b => ESumLeft a b (execLeft a) *)
+(*   | Prod a b => EProd a b (execLeft a) (execLeft b) *)
+(*   end *)
+(*   . *)
 
 (* a is less than b. If b is executed, then a is also executed *)
 Definition projLe a b : Prop := Exec b -> Exec a.
 
-Notation "a '<=' b" := (projLe a b) (at level 70).
+Bind Scope project with Project.
+Delimit Scope project with proj.
+Open Scope project.
 
-Notation "a '>=' b" := (projLe b a) (at level 70).
+Notation "a ':+' b" := (Sum a b) (right associativity, at level 55) : project.
 
-Notation "a '<=>' b" := ((projLe a b) /\ (projLe b a)) (at level 70).
+Notation "a ':*' b" := (Prod a b) (right associativity, at level 45) : project.
+
+Notation "a '<=' b" := (projLe a b) (at level 70) : project.
+
+(* Notation "a '>=' b" := (projLe b a) (at level 70) : project. *)
+
+Notation "a '<=>' b" := ((projLe a b) /\ (projLe b a)) (at level 70) : project.
 
 Lemma leTransitive : forall a b c, a <= b -> b <= c -> a <= c.
 Proof.
@@ -40,19 +44,76 @@ Proof.
   auto.
 Qed.
 
-Lemma sumSymmetry : forall a b, a + b <= b + a.
+Lemma sumSymmetry : forall a b, a :+ b <=> b :+ a.
 Proof.
   unfold projLe.
-  intros.
-  inversion H ; subst.
-  - apply ESumRight. assumption.
-  - apply ESumLeft. assumption.
+  split ;
+  intros ;
+  inversion H ; subst ;
+    try (apply ESumRight ; assumption ) ;
+    try (apply ESumLeft ; assumption ) .
 Qed.
 
-Lemma prodSymmetry : forall a b, a * b <= b * a.
+Lemma sumTransitive : forall a b c, a :+ (b :+ c) <= (a :+ b) :+ c .
 Proof.
   unfold projLe.
   intros.
-  inversion H ; subst.
-  constructor ; assumption.
+  inversion H ; subst. {
+    inversion H1 ; subst.
+    - apply ESumLeft. assumption.
+    - apply ESumRight. apply ESumLeft. assumption.
+  } {
+    apply ESumRight. apply ESumRight. assumption.
+  }
+Qed.
+
+Lemma prodSymmetry : forall a b, a :* b <=> b :* a.
+Proof.
+  unfold projLe.
+  split;
+    intros ;
+    inversion H ; subst;
+    constructor ; assumption.
+Qed.
+
+Lemma prodTransitive : forall a b c, a :* (b :* c) <=> (a :* b) :* c .
+Proof.
+  unfold projLe.
+  split. {
+    intros.
+    inversion H ; subst.
+    inversion H2 ; subst.
+    constructor ; try assumption ; try (constructor ; assumption).
+  } {
+    intros.
+    inversion H ; subst.
+    inversion H3 ; subst.
+    constructor ; try assumption ; try (constructor ; assumption).
+  }
+Qed.
+
+Lemma sumDistrib : forall a b c, a :* (b :+ c) <=> a :* b :+ a :* c.
+Proof.
+  unfold projLe.
+  split. {
+    intros.
+    inversion H ; subst. {
+      inversion H1 ; subst.
+      - constructor ; try assumption.
+        apply ESumLeft. assumption.
+    } {
+      inversion H1 ; subst.
+      constructor ; try assumption.
+      apply ESumRight. assumption.
+    }
+  } {
+    intros.
+    inversion H ; subst. {
+      inversion H3 ; subst. {
+        apply ESumLeft. constructor ; assumption.
+      } {
+        apply ESumRight. constructor ; assumption.
+      }
+    }
+  }
 Qed.
